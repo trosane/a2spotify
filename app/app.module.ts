8d78b0cafe8c8d916e072fa3908f147608a6a57f
Trent Rosane <trosane@uw.edu>
2016-11-15 20:39:37 -0800

    final w/o region filter

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumComponent } from './components/album/album.component';
+import { routing, appRoutingProviders } from './app.routes';
+import { FormsModule } from '@angular/forms';
+import { SpotifyService } from './services/spotify.service';
+import { Http, Headers, HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ BrowserModule, routing, FormsModule, HttpModule ],
+  declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent, ArtistComponent, AlbumComponent ],
+  providers: [ appRoutingProviders, SpotifyService],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { appRoutingProviders } from './app.routes';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule, appRoutingProviders);
